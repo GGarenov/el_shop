93e098d84b121fcc95b5f9e01ebf33642f7980c3
Georgiy Garenov <g_garenov@outlook.com>
2023-10-22 19:54:14 +0300

    1. Install express JS and handlebars 2. Make an architecture - src folder, public folder with css and images and views folder. 3. Make main.hbs and home.hbs 4. Basics in index.js - The starting point     - Make basic express config to read static files.     - create router.js which is still empty but later I will move some code inside

---
+++ readme.md
@@ -0,0 +1,6 @@
+1. Install express JS and handlebars
+2. Make an architecture - src folder, public folder with css and images and views folder.
+3. Make main.hbs and home.hbs
+4. Basics in index.js - The starting point
+    - Make basic express config to read static files.
+    - create router.js which is still empty but later I will move some code inside
---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+//Express Config
+app.use(express.static(path.resolve(__dirname, "./public"))); // this find static files
+
+//Handlebars Config
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
