6d479ae132607e3aaf369ea9c1aab5465160abee
Georgiy Garenov <g_garenov@outlook.com>
2023-10-26 21:04:53 +0300

    Make emaild and password validation with bcrypt

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 exports.register = async (userData) => {
   const user = await User.create(userData);
@@ -12,4 +13,11 @@ exports.login = async (email, password) => {
   if (!user) {
     throw new Error("Invalid email or password");
   }
+
+  //validate password
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) {
+    throw new Error("Invalid email or password");
+  }
+  return user;
 };
